220cf94e4ec40ff98d80d87d2c3e4287cf9baf0e
omygoddess <66275674+omygoddess@users.noreply.github.com>
2021-12-12 21:00:53 +0300

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -19,14 +19,3 @@ def execute_query(connection, query, params):
         print("Query executed successfully")
     except Error as e:
         print(f"The error '{e}' occurred")
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
